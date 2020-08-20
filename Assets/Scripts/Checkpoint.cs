78d5341d92da3876648e2e2103a697878f7b787b
Jopet Agcaoili <jopz@secret6.com>
2020-08-21 01:39:25 +0800

    - appearing boat added - path added

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -6,11 +6,21 @@ public class Checkpoint : MonoBehaviour
 {
     [SerializeField]Transform m_checkpoint;
 	[SerializeField]GameObject m_object;
+	[SerializeField]bool m_enableObject;
+	[SerializeField]List<GameObject> m_objectsToEnable = new List<GameObject>(); 
 
 	void OnTriggerEnter(Collider col) 
 	{
 		if (col.tag == "Player") {
 			CheckpointManager.Instance.CurrentCheckpoint = m_checkpoint;
+			
+			if (m_enableObject)
+			{
+				for (int i = 0; i < m_objectsToEnable.Count; i++)
+				{
+					m_objectsToEnable[i].SetActive(true);
+				}
+			}
 			m_object.SetActive(false);
 		}
 	}
---
+++ Assets/Scripts/VehicleManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class VehicleManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+	
+    float m_elapsedTime = 0;
+	[SerializeField]float m_speed;
+
+    [SerializeField] PathCreator m_pathCreator = null;
+	[SerializeField]private float m_vehicleLength = 40f;
+
+    private const float DISTANCE_START = 20f;
+
+    [SerializeField] List<GameObject> m_vehicles = new List<GameObject>();
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < m_vehicles.Count; i++)
+		{
+			GameObject vehicle = m_vehicles[i];
+			m_elapsedTime += Time.deltaTime * m_speed;
+
+            float t = DISTANCE_START + m_elapsedTime - m_vehicleLength * i;
+			vehicle.transform.position = m_pathCreator.path.GetPointAtDistance(t);
+            vehicle.transform.rotation = m_pathCreator.path.GetRotationAtDistance(t) * Quaternion.Euler(0, 0, 0);
+		}
+    }
+}
