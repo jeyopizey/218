a462520312f47515ffba6183fd26dd2c62d32f06
Jopet Agcaoili <jopz@secret6.com>
2020-08-25 00:26:09 +0800

    scene transition added

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+	void OnTriggerEnter (Collider p_col)
+	{
+		if (p_col.tag == "Player") {
+			LevelManager.Instance.LoadNextLevel();
+		}
+	}
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoInstance<LevelManager>
+{
+	public void LoadNextLevel()
+	{
+		GameController.Instance.CurrentStage += 1;
+		GameController.Instance.StartStage();
+		SceneManager.LoadScene("Stage"+ (GameController.Instance.CurrentStage + 1) );
+	}
+}
